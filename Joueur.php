b0bc02817921c9777b80afd16f7ef5ae474fa7f6
vsk49 <virgilskoh@gmail.com>
2024-11-15 10:18:48 +0100

    Changement de nomenclature des classes

--- Joueur.php
+++ Joueur.php
@@ -5,15 +5,15 @@
     class Joueur {
 
         private $id;
+        private $numeroLicense;
         private $nom;
         private $prenom;
         private $dateNaissance;
-        private $numeroLicense;
         private $taille;
         private $poids;
-        private Note $note;
+        private Statut $statut;
 
-        public function  __construct(int $id, string $nom, string $prenom, string $dateNaissance, int $numeroLicense, int $taille, int $poids, Note $note) {
+        public function  __construct(int $id, string $nom, string $prenom, string $dateNaissance, int $numeroLicense, int $taille, int $poids, Statut $statut) {
             $this -> id = $id;
             $this -> nom = $nom;
             $this -> prenom = $prenom;
@@ -21,7 +21,35 @@
             $this -> numeroLicense = $numeroLicense;
             $this -> taille = $taille;
             $this -> poids = $poids;
-            $this -> note = $note;
+            $this -> statut = $statut;
+        }
+
+        public function getId(): int {
+            return $this -> id;
+        }
+
+        public function getNom(): string {
+            return $this -> nom;
+        }
+
+        public function setNom(string $nom): void {
+            $this -> nom = $nom;
+        }
+
+        public function getPrenom(): string {
+            return $this -> prenom;
+        }
+
+        public function setPrenom(string $prenom): void {
+            $this -> prenom = $prenom;
+        }
+
+        public function getDateNaissance(): string {
+            return $this -> dateNaissance;
+        }
+
+        public function getNumeroLicense(): String {
+            return $this -> numeroLicense;
         }
 
     }
