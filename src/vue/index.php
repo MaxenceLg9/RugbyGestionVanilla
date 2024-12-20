24a8be0297f38a787de685c3b8b6833149b2897f
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2024-12-20 10:36:34 +0100

    Forwarding on the project. Rebased virgil's branch into mine. Adding css and modifying index page.

--- src/vue/index.php
+++ src/vue/index.php
@@ -6,16 +6,25 @@
     <link rel="stylesheet" href="../resources/style/style.css"/>
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="">Mon équipe</a></li>
-            <li><a href="">Résultats</a></li>
-            <li><a href="">Mon profil</a></li>
-            <li><a href="">Se déconnecter</a></li>
-        </ul>
-    </nav>
-    <h1>
-        Bienvenue <?php echo $post['nom'] ?>
-    </h1>
-    <p></p>
+<nav>
+    <ul>
+        <li><a href="#">Mon équipe</a></li>
+        <li><a href="#">Résultats</a></li>
+        <li><a href="#">Mon profil</a></li>
+    </ul>
+    <ul>
+        <li><a href="#">Se déconnecter</a></li>
+    </ul>
+</nav>
+<div class="main">
+    <section>
+        <h1>
+            Bienvenue <?php echo $post['nom'] ?>
+        </h1>
+        <p></p>
+    </section>
+    <section>
+        <p>Voir vos derniers résultats</p>
+    </section>
+</div>
 </body>
