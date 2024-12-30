89499801903608d6dbcc2091f83d06b88c143c27
Lagourgue Maxence <maxence.lagourgue@free.fr>
2024-12-31 00:21:08 +0100

    petite page de connexion simple.

--- src/controleur/login.php
+++ src/controleur/login.php
@@ -1,3 +1,18 @@
 <?php
-
-echo "C";
+require_once "../db/DAOEntraineur.php";
+if(!DAOEntraineur::existEntraineur()) {
+    header('Location: http://rugbygestionplus.com/register');
+//    die();
+}
+if(!empty($_POST) && isset($_POST["email"])){
+    $entraineur = Entraineur::getEntraineur($_POST["email"],$_POST["password"]);
+    if($entraineur == null){
+        header('location : login');
+        setcookie("error","invalid user");
+        die();
+    }
+    session_start();
+    $_SESSION["email"] = $entraineur->getEmail();
+    $_SESSION["id"] = $entraineur->getIdEntraineur();
+}
+include "../vue/login.php";
