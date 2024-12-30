6582fede7c1a4b210b82b8c88fab14cbca3f2ecb
Lagourgue Maxence <maxence.lagourgue@free.fr>
2024-12-31 00:22:22 +0100

    séparation en tant que composant de la nav bar

---
+++ src/components/nav.php
@@ -0,0 +1,12 @@
+<?php require "../modele/Urls.php" ?>
+<nav>
+    <ul>
+        <li><a href="<?php echo $URL['team']?>">Mon équipe</a></li>
+        <li><a href="<?php echo $URL['matchs']?>">Matchs</a></li>
+        <li><a href="<?php echo $URL['results']?>">Résultats</a></li>
+        <li><a href="<?php echo $URL['me']?>">Mon profil</a></li>
+    </ul>
+    <ul>
+        <li><a href="<?php echo $URL['logout']?>">Se déconnecter</a></li>
+    </ul>
+</nav>
