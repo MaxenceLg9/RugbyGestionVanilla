79d75e15d58f5a873a53d06da2a4f1ff4ec4d9c7
Lagourgue Maxence <maxence.lagourgue@free.fr>
2024-12-31 16:27:29 +0100

    refactoring

--- src/modele/MatchDeRugby.php
+++ src/modele/MatchDeRugby.php
@@ -1,74 +1,73 @@
 <?php
 
-    require '../modele/Resultat.php';
-    require '../modele/Lieu.php';
-    include '../db/DAOMatchDeRugby.php';
+require '../modele/Resultat.php';
+require '../modele/Lieu.php';
+include '../db/DAOMatchDeRugby.php';
 
-    class MatchDeRugby {
+class MatchDeRugby {
 
-        private ?int $idMatchDeRugby;
-        private DateTime $dateHeure;
-        private string $adversaire;
-        private Lieu $lieu;
-        private ?Resultat $resultat;
+    private ?int $idMatchDeRugby;
+    private DateTime $dateHeure;
+    private string $adversaire;
+    private Lieu $lieu;
+    private ?Resultat $resultat;
 
-        function __construct(int $idMatchDeRugby, DateTime $dateHeure, string $adversaire, Lieu $lieu) {
-            $this -> idMatchDeRugby = $idMatchDeRugby;
-            $this -> dateHeure = $dateHeure;
-            $this -> adversaire = $adversaire;
-            $this -> lieu = $lieu;
-        }
-
-        public static function getFromId(int $idMatch): MatchDeRugby
-        {
-            return DAOMatchDeRugby::readById($idMatch);
-        }
-
-        public function getIdMatchDeRugby(): int {
-            return $this -> idMatchDeRugby;
-        }
-
-        public function getDateHeure(): DateTime {
-            return $this -> dateHeure;
-        }
-
-        public function getAdversaire(): string {
-            return $this -> adversaire;
-        }
-
-        public function getLieu(): Lieu {
-            return $this -> lieu;
-        }
-
-        public function getResultat(): Resultat {
-            return $this -> resultat;
-        }
-
-        public function setResultat(Resultat $resultat): void {
-            $this -> resultat = $resultat;
-        }
-
-        // partie DAO : utilisation des méthodes de la classe DAOMatchDeRugby
-        public function saveMatchDeRugby(): void {
-            $daoMatchDeRugby = new DAOMatchDeRugby();
-            if ($daoMatchDeRugby -> readByDateHeure($this -> dateHeure) === null) {
-                $daoMatchDeRugby -> create($this);
-            }
-            $daoMatchDeRugby -> update($this);
-        }
-
-        public static function getAllMatchDeRugby(): array {
-            return DAOMatchDeRugby::read();
-        }
-
-        public function getMatchDeRugbyByDateHeure(): MatchDeRugby {
-            $daoMatchDeRugby = new DAOMatchDeRugby();
-            return $daoMatchDeRugby -> readByDateHeure($this -> dateHeure);
-        }
-
-        public function deleteMatchDeRugby(): void {
-            $daoMatchDeRugby = new DAOMatchDeRugby();
-            $daoMatchDeRugby -> delete($this);
-        }
-
+    function __construct(int $idMatchDeRugby, DateTime $dateHeure, string $adversaire, Lieu $lieu) {
+        $this -> idMatchDeRugby = $idMatchDeRugby;
+        $this -> dateHeure = $dateHeure;
+        $this -> adversaire = $adversaire;
+        $this -> lieu = $lieu;
     }
+
+    public static function getFromId(int $idMatch): MatchDeRugby
+    {
+        return DAOMatchDeRugby::readById($idMatch);
+    }
+
+    public function getIdMatchDeRugby(): int {
+        return $this -> idMatchDeRugby;
+    }
+
+    public function getDateHeure(): DateTime {
+        return $this -> dateHeure;
+    }
+
+    public function getAdversaire(): string {
+        return $this -> adversaire;
+    }
+
+    public function getLieu(): Lieu {
+        return $this -> lieu;
+    }
+
+    public function getResultat(): Resultat {
+        return $this -> resultat;
+    }
+
+    public function setResultat(Resultat $resultat): void {
+        $this -> resultat = $resultat;
+    }
+
+    // partie DAO : utilisation des méthodes de la classe DAOMatchDeRugby
+    public function saveMatchDeRugby(): void {
+        $daoMatchDeRugby = new DAOMatchDeRugby();
+        if ($daoMatchDeRugby -> readByDateHeure($this -> dateHeure) === null) {
+            $daoMatchDeRugby -> create($this);
+        }
+        $daoMatchDeRugby -> update($this);
+    }
+
+    public static function getAllMatchDeRugby(): array {
+        return DAOMatchDeRugby::read();
+    }
+
+    public function getMatchDeRugbyByDateHeure(): MatchDeRugby {
+        $daoMatchDeRugby = new DAOMatchDeRugby();
+        return $daoMatchDeRugby -> readByDateHeure($this -> dateHeure);
+    }
+
+    public function deleteMatchDeRugby(): void {
+        $daoMatchDeRugby = new DAOMatchDeRugby();
+        $daoMatchDeRugby -> delete($this);
+    }
+}
