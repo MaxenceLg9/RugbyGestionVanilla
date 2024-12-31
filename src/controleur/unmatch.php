a22c10bd0ca84440a17a2c47c5daa2722364a36a
Lagourgue Maxence <maxence.lagourgue@free.fr>
2024-12-31 16:49:34 +0100

    modification type ajout

--- src/controleur/unmatch.php
+++ src/controleur/unmatch.php
@@ -6,6 +6,9 @@ if(isset($_POST)){
     if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
         die('CSRF validation failed.');
     }
+    if(!isset($_POST["type"])){
+        die("Type de requête non défini");
+    }
     if($_POST["type"] == "ajout") {
         $match = new MatchDeRugby(-1, $_POST['dateHeure'], $_POST['adversaire'], $_POST['lieu']);
         $match->saveMatchDeRugby();
@@ -17,6 +20,15 @@ if(isset($_POST)){
         }
         $match = MatchDeRugby::getFromId($_POST['idMatch']);
         $match->deleteMatchDeRugby();
+    }
+    elseif ($_POST["type"] == "modification") {
+        // Sanitize and validate the match ID
+        if (!isset($_POST['idMatch']) || !is_numeric($_POST['idMatch'])) {
+            die('Invalid match ID.');
+        }
+        $match = MatchDeRugby::getFromId($_POST['idMatch']);
+        $match->setResultat($_POST['resultat']);
+        $match->saveMatchDeRugby();
     }
 }
 include '../vue/nouveaumatch.php';
