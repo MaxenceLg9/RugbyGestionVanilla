0a4d5846def82d20c0b792dad561961c6bab2e3c
Lagourgue Maxence <maxence.lagourgue@free.fr>
2024-12-31 16:47:49 +0100

    visualisation des matchs prévus. A faire donc la creation/modif/supression.

--- src/controleur/matchs.php
+++ src/controleur/matchs.php
@@ -6,45 +6,8 @@ if(empty($_SESSION['email'])){
 }
 
 require_once "../modele/MatchDeRugby.php";
-$matchs = [
-    [
-        'idMatchDeRugby' => -1,
-        'dateHeure' => "2021-01-01 14:30:00",
-        'adversaire' => "RC Valence",
-        'lieu' => "Stade de Mondimrade",
-        'resultat' => "futur",
-    ],
-    [
-        'idMatchDeRugby' => -1,
-        'dateHeure' => "2021-01-01 14:30:00",
-        'adversaire' => "RC Valence",
-        'lieu' => "Stade de Mondimrade",
-        'resultat' => "futur",
-    ],
-    [
-        'idMatchDeRugby' => -1,
-        'dateHeure' => "2021-01-01 14:30:00",
-        'adversaire' => "RC Valence",
-        'lieu' => "Stade de Mondimrade",
-        'resultat' => "futur",
-    ],
-    [
-        'idMatchDeRugby' => -1,
-        'dateHeure' => "2021-01-01 14:30:00",
-        'adversaire' => "RC Valence",
-        'lieu' => "Stade de Mondimrade",
-        'resultat' => "futur",
-    ],
-    [
-        'idMatchDeRugby' => -1,
-        'dateHeure' => "2021-01-01 14:30:00",
-        'adversaire' => "RC Valence",
-        'lieu' => "Stade de Mondimrade",
-        'resultat' => "futur",
-    ]
-];
 
 $matchs = MatchDeRugby::getAllMatchDeRugby();
-$css = ["style.css"];
+$css = ["style.css","matchs.css"];
 $page = "../vue/matchs.php";
 include_once "../components/page.php";
--- src/vue/matchs.php
+++ src/vue/matchs.php
@@ -2,16 +2,25 @@
 
 <div class="main">
     <?php
+    $match = new MatchDeRugby(1, new DateTime(), "Toulouse", Lieu::DOMICILE);
     foreach($matchs as $match){?>
         <section>
-            <h2>Match du <?php echo $match['dateHeure']?></h2>
-            <p>Adversaire: <?php echo $match['adversaire']?></p>
-            <p>Lieu: <?php echo $match['lieu']?>"</p>
-            <p>Résultat: <?php echo $match['resultat']?></p>
+            <div>
+                <h2>Match du <?php echo $match->getDateHeure()->format('d/m/Y-H:i')?></h2>
+                <p>Adversaire: <?php echo $match->getAdversaire()?></p>
+                <p>Lieu: <?php echo $match->getLieu()->name?></p>
+            </div>
             <form method="POST" action="matchs.php" style="margin-top: 10px;">
-                <input type="hidden" name="idMatch" value="<?= htmlspecialchars($match['idMatch']) ?>">
+                <input type="hidden" name="type" value="modifiy">
+                <input type="hidden" name="idMatch" value="<?= htmlspecialchars($match->getIdMatchDeRugby()) ?>">
                 <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($_SESSION['csrf_token']) ?>">
-                <button type="submit" style="background-color: red; color: white;">Delete</button>
+                <button type="submit" class="modify">Modifier le match</button>
+            </form>
+            <form method="POST" action="matchs.php" style="margin-top: 10px;">
+                <input type="hidden" name="type" value="delete">
+                <input type="hidden" name="idMatch" value="<?= htmlspecialchars($match->getIdMatchDeRugby()) ?>">
+                <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($_SESSION['csrf_token']) ?>">
+                <button type="submit" class="delete">Supprimer le match</button>
             </form>
         </section>
         <?php
