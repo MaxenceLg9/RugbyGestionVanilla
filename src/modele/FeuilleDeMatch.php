fe9cdeef14f5cfd589cb36065a13a8e14d8413fe
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-01-08 15:21:56 +0100

    FeuilleDeMatch est lié à un match et une liste de joueurs

--- src/modele/FeuilleDeMatch.php
+++ src/modele/FeuilleDeMatch.php
@@ -1,90 +1,36 @@
 <?php
 
-    require '../modele/Resultat.php';
-    require '../modele/Lieu.php';
-    require '../modele/MatchDeRugby.php';
-    require '../modele/Joueur.php';
-    include '../db/DAOFeuilleDeMatch.php';
+require_once '../modele/Resultat.php';
+require_once '../modele/Lieu.php';
+require_once '../modele/MatchDeRugby.php';
+require_once '../modele/Joueur.php';
+require_once '../db/DAOJouerUnMatch.php';
 
-    class FeuilleDeMatch {
+class FeuilleDeMatch {
 
-        private int $idMatchDeRugby;
-        private int $idJoueur;
-        private bool $estTitulaire;
-        private string $poste;
-        private float $note;
+    //TODO : to check the logic of everything
+    private int $idMatch;
+    private array $joueurs;
 
-        function __construct(int $matchDeRugby, int $joueur, bool $estTitulaire, string $poste, float $note) {
-            $this -> idMatchDeRugby = $matchDeRugby;
-            $this -> idJoueur = $joueur;
-            $this -> estTitulaire = $estTitulaire;
-            $this -> poste = $poste;
-            $this -> note = $note;
-        }
-
-        public function getIdMatchDeRugby() : int {
-            return $this -> idMatchDeRugby;
-        }
-
-        public function getIdJoueur() : int {
-            return $this -> idJoueur;
-        }
-
-        public function estJoueurTitulaire() : bool {
-            return $this -> estTitulaire;
-        }
-
-        public function getPoste() : string {
-            return $this -> poste;
-        }
-
-        public function setPoste(string $poste): void
-        {
-            $this -> poste = $poste;
-        }
-
-        public function setEstTitulaire(bool $estTitulaire): void
-        {
-            $this -> estTitulaire = $estTitulaire;
-        }
-
-        public function getNote() : float {
-            return $this -> note;
-        }
-
-        public function setNote(float $note): void {
-            $this -> note = $note;
-        }
-
-        // partie DAO
-        public function saveFeuilleDeMatch(): void {
-            $daoFeuilleDeMatch = new DAOFeuilleDeMatch();
-            if ($daoFeuilleDeMatch -> read($this -> idMatchDeRugby, $this -> idJoueur) != null) {
-                $daoFeuilleDeMatch -> update($this);
-            }
-            $daoFeuilleDeMatch -> create($this);
-        }
-
-        public function deleteFeuilleDeMatch(): void {
-            $daoFeuilleDeMatch = new DAOFeuilleDeMatch();
-            if ($daoFeuilleDeMatch -> read($this -> idMatchDeRugby, $this -> idJoueur) != null) {
-                $daoFeuilleDeMatch->delete($this);
-            }
-        }
-
-        public function getAllFeuilleDeMatchByMatch(): array {
-            $daoFeuilleDeMatch = new DAOFeuilleDeMatch();
-            return $daoFeuilleDeMatch -> readAllByMatch($this->getIdMatchDeRugby());
-        }
-
-        public function getAllFeuilleDeMatchByJoueur(): array {
-            $daoFeuilleDeMatch = new DAOFeuilleDeMatch();
-            return $daoFeuilleDeMatch -> readAllByJoueur($this->getIdJoueur());
-        }
-
-        public static function getAllFeuilleDeMatch(): array {
-            $daoFeuilleDeMatch = new DAOFeuilleDeMatch();
-            return $daoFeuilleDeMatch -> readAll();
-        }
-
+    function __construct(int $matchDeRugby) {
+        $this -> $idMatch = $matchDeRugby;
+        $this -> joueurs = DAOJouerUnMatch::readAllByMatch(MatchDeRugby::getFromId($idMatch));
     }
+
+    public function getIdMatch() : int {
+        return $this -> idMatch;
+    }
+
+    public function addJoueur(JouerUnMatch $joueur): void {
+        $this -> joueurs[$joueur->getJoueur()->getIdJoueur()] = $joueur;
+    }
+
+    public function removeJoueur(JouerUnMatch $joueur): void {
+        unset($this -> joueurs[$joueur->getJoueur()->getIdJoueur()]);
+    }
+
+    public function getJoueurs() : array {
+        return $this -> joueurs;
+    }
+
+}
