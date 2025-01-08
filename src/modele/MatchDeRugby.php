22d95ed11ab788fc3ae51d1c5e84c862e758c36f
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-01-08 14:57:16 +0100

    nouveaux setters pour la date, l'adversaire & le lieu

--- src/modele/MatchDeRugby.php
+++ src/modele/MatchDeRugby.php
@@ -28,14 +28,35 @@ class MatchDeRugby {
         return $this -> idMatchDeRugby;
     }
 
+
+    /**
+     * @param DateTime $dateHeure
+     */
+    public function setDateHeure(DateTime $dateHeure): void
+    {
+        $this->dateHeure = $dateHeure;
+    }
+
     public function getDateHeure(): DateTime {
         return $this -> dateHeure;
+    }
+
+    public function setAdversaire(string $adversaire): void
+    {
+        $this->adversaire = $adversaire;
     }
 
     public function getAdversaire(): string {
         return $this -> adversaire;
     }
 
+    /**
+     * @param Lieu $lieu
+     */
+    public function setLieu(Lieu $lieu): void
+    {
+        $this->lieu = $lieu;
+    }
     public function getLieu(): Lieu {
         return $this -> lieu;
     }
@@ -51,14 +72,18 @@ class MatchDeRugby {
     // partie DAO : utilisation des méthodes de la classe DAOMatchDeRugby
     public function saveMatchDeRugby(): void {
         $daoMatchDeRugby = new DAOMatchDeRugby();
-        if ($daoMatchDeRugby -> readByDateHeure($this -> dateHeure) == null) {
+        if ($this->getIdMatchDeRugby() === -1) {
             $daoMatchDeRugby -> create($this);
             return;
         }
         $daoMatchDeRugby -> update($this);
     }
 
-    public static function getAllMatchDeRugby(): array {
+    public function setResult(): void{
+        (new DAOMatchDeRugby()) -> setResult($this);
+    }
+
+    public static function findAll(): array {
         return DAOMatchDeRugby::read();
     }
 
