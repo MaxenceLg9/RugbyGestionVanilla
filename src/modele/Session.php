b0b2d6071f10f621bb9c1e3bde80d13f21747781
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-01-08 12:26:42 +0100

    gestion de Session

---
+++ src/modele/Session.php
@@ -0,0 +1,22 @@
+<?php
+function checkSession(): void
+{
+    session_start();
+    if (empty($_SESSION['email'])) {
+        header('Location: login');
+        destroySession();
+        die();
+    }
+}
+function destroySession(): void
+{
+    $_SESSION = [];
+    session_destroy();
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+}
