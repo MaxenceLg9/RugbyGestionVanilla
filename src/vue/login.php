bcdf4fa5e7d8fc44a0e790555915e73e8f52ca83
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-01-08 16:51:11 +0100

    vue de connexion finie

--- src/vue/login.php
+++ src/vue/login.php
@@ -1,16 +1,18 @@
-<div class="login">
-    <h1>Connexion</h1>
-    <form method="post" action="/login" class="login">
-        <div>
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" required>
+<div class="background">
+    <div class="login">
+        <h1>Connexion</h1>
+        <form method="post" action="/login" class="login">
+            <div>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required>
 
-            <label for="password">Mot de passe</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <?php if(isset($_COOKIE['login_error'])): ?>
-            <p class="error"><?= $_COOKIE['login_error'] ?></p>
-        <?php endif; ?>
-        <input type="submit" value="Se connecter">
-    </form>
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <?php if(isset($_COOKIE['login_error'])): ?>
+                <p class="error"><?= $_COOKIE['login_error'] ?></p>
+            <?php endif; ?>
+            <input type="submit" value="Se connecter">
+        </form>
+    </div>
 </div>
--- src/vue/register.php
+++ src/vue/register.php
@@ -1,30 +1,32 @@
-<div class="login">
-    <h1>Inscription</h1>
-    <form method="post" action="../controleur/register.php" class="login">
-        <div>
-            <label for="firstname">Prenom</label>
-            <input type="text" id="firstname" name="firstname" required>
+<div class="background">
+    <div class="login">
+        <h1>Inscription</h1>
+        <form method="post" action="../controleur/register.php" class="login">
+            <div>
+                <label for="firstname">Prenom</label>
+                <input type="text" id="firstname" name="firstname" required>
 
-            <label for="name">Nom</label>
-            <input type="text" id="name" name="name" required>
+                <label for="name">Nom</label>
+                <input type="text" id="name" name="name" required>
 
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" required>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required>
 
-            <label for="equipe">Nom de l'equipe</label>
-            <input type="text" id="equipe" name="equipe" required>
+                <label for="equipe">Nom de l'equipe</label>
+                <input type="text" id="equipe" name="equipe" required>
 
-            <label for="password">Mot de passe</label>
-            <input type="password" id="password" name="password" required>
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" name="password" required>
 
-            <label for="confirmpassword">Confirmer le mot de passe</label>
-            <input type="password" id="password" name="confirmpassword" required>
-        </div>
-        <?php if(isset($_COOKIE["register_error"])){ ?>
-            <div class="error">
-                <?php echo $_COOKIE["register_error"]; ?>
+                <label for="confirmpassword">Confirmer le mot de passe</label>
+                <input type="password" id="password" name="confirmpassword" required>
             </div>
-        <?php } ?>
-        <input type="submit" value="Se connecter">
-    </form>
+            <?php if(isset($_COOKIE["register_error"])){ ?>
+                <div class="error">
+                    <?php echo $_COOKIE["register_error"]; ?>
+                </div>
+            <?php } ?>
+            <input type="submit" value="Se connecter">
+        </form>
+    </div>
 </div>
