eb146eb413c5d1c6e776e1181236fb99ce947509
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-01-09 16:40:46 +0100

    changement de l'url avec le nom

--- src/components/nav.php
+++ src/components/nav.php
@@ -2,7 +2,7 @@
 <nav>
     <ul>
         <li><a href="<?php echo $URL['accueil']?>">Accueil</a></li>
-        <li><a href="<?php echo $URL['team']?>">Mon équipe</a></li>
+        <li><a href="<?php echo $URL['equipe']?>">Mon équipe</a></li>
         <li><a href="<?php echo $URL['matchs']?>">Matchs</a></li>
         <li><a href="<?php echo $URL['results']?>">Résultats</a></li>
         <li><a href="<?php echo $URL['me']?>">Mon profil</a></li>
