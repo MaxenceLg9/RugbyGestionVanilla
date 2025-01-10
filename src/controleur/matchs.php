c29e35a62a4adee7fef2bd2b7f617f9ea84596aa
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-01-10 23:16:55 +0100

    import du modèle JouerUnMatch

--- src/controleur/matchs.php
+++ src/controleur/matchs.php
@@ -3,6 +3,7 @@ require_once "../modele/Session.php";
 checkSession();
 
 require_once "../modele/MatchDeRugby.php";
+require_once "../modele/JouerUnMatch.php";
 
 $matchs = MatchDeRugby::findAll();
 $title = "Tous les matchs";
